df4eb7b2a75846855ee65d5b83fd1e5107df3891
encoderit-rafi <rafi@encoderit.net>
2026-01-19 20:16:18 +0600

    css update

--- src/routes/_quiz/result/index.tsx
+++ src/routes/_quiz/result/index.tsx
@@ -10,15 +10,14 @@ export const Route = createFileRoute('/_quiz/result/')({
 })
 
 function RouteComponent() {
-  const { quizId, getTotalMarks, setResultPageId } = useQuizStore()
+  const { quiz, getTotalMarks, setResultPageId } = useQuizStore()
   const totalMarks = getTotalMarks()
-
   const {
     data: resultData,
     isPending,
-    // isError,
+    isError,
   } = useGetResultPage({
-    quiz_id: quizId!,
+    quiz_id: quiz?.uuid || '',
     mark: totalMarks,
   })
 
@@ -40,30 +39,30 @@ function RouteComponent() {
     )
   }
 
-  // if (isError) {
-  //   return (
-  //     <div className="flex flex-col items-center justify-center min-h-[50vh] text-center px-4">
-  //       <h2 className="text-2xl font-bold text-red-500">
-  //         Oops! Something went wrong.
-  //       </h2>
-  //       <p className="mt-2 text-gray-600">
-  //         We couldn't retrieve your results. Please try again later.
-  //       </p>
-  //     </div>
-  //   )
-  // }
+  if (isError) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-[50vh] text-center px-4">
+        <h2 className="text-2xl font-bold text-red-500">
+          Oops! Something went wrong.
+        </h2>
+        <p className="mt-2 text-gray-600">
+          We couldn't retrieve your results. Please try again later.
+        </p>
+      </div>
+    )
+  }
 
   const result = resultData?.data || resultData
 
   return (
-    <div className="">
+    <div className="mx-auto my-10 max-w-2xl px-4">
       <div
-        className="prose"
+        className="prose prose-slate prose-custom max-w-none prose-headings:text-(--secondary-color) mb-10 prose-table:border prose-table:border-(--primary-color)/20 prose-th:border prose-th:border-(--primary-color)/20 prose-th:p-4 prose-td:border prose-td:border-(--primary-color)/20 prose-td:p-4"
         dangerouslySetInnerHTML={{
           __html: result?.content || '',
         }}
       />
-      <div className="flex justify-center">
+      <div className="flex justify-center my-4">
         <Link
           to="/submission"
           className={buttonVariants({
